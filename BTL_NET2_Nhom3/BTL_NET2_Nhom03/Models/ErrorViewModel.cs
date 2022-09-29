221caebae07a8c893a24782db464a8252c679023
Vu_Linh_ThinkPadE14 <Vu_Linh_ThinkPadE14@THINKPADE14>
2022-09-29 21:11:45 +0700

    Commit_Node

---
+++ BTL_NET2_Nhom3/BTL_NET2_Nhom03/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BTL_NET2_Nhom03.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
